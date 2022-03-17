2e554eb02c0bc59bfb1900e44652899cc97eefb6
vjspranav <pranavasri@live.in>
2022-03-18 00:06:40 +0530

    Extra: Extend Partial Evaluator for Lif

--- compiler.rkt
+++ compiler.rkt
@@ -349,13 +349,14 @@
   (match e
     [(Int n) (Int n)]
     [(Var x) (Var x)]
+    [(Bool t) (Bool t)]
     [(Prim 'read '()) (Prim 'read '())]
     [(Prim '- (list e1)) (pe_neg (pe_exp e1))]
     [(Prim '+ (list (Int e1) e2)) (pe_add (Int e1) (pe_exp e2))]
     [(Prim '+ (list e1 (Int e2) )) (pe_add (Int e2) (pe_exp e1))]
     [(Prim '+ (list e1 e2)) (pe_add (pe_exp e1) (pe_exp e2))]
-    [(Let x e1 e2) (Let x (pe_exp e1) (pe_exp e2))]))
-
+    [(Let x e1 e2) (Let x (pe_exp e1) (pe_exp e2))]
+    [(If e1 e2 e3) (If (pe_exp e1) (pe_exp e2) (pe_exp e3))]))
 
 ; Add a pe_intelligent_eval that evaluates the function from left to right
 ; and then evaluates the result
@@ -363,14 +364,16 @@
   (match e
     [(Int n) (Int n)]
     [(Var x) (Var x)]
+    [(Bool t) (Bool t)]
     [(Prim 'read '()) (Prim 'read '())]
     [(Prim '- (list e1)) (pe_neg (pe_intelligent_exp e1))]
     [(Prim '+ (list (Int e1) (Prim '+ (list (Int e2) e3)))) (pe_add (pe_add (Int e1) (Int e2)) (pe_intelligent_exp e3))]
     [(Prim '+ (list (Prim '+ (list (Int e1) e2)) (Prim '+ (list (Int e3) e4)))) (pe_add (pe_add (Int e1) (Int e3)) (pe_add (pe_intelligent_exp e2) (pe_intelligent_exp e4)))]
     [(Prim '+ (list e1 e2)) (pe_add (pe_intelligent_exp e1) (pe_intelligent_exp e2))]
-    [(Let x e1 e2) (Let x (pe_intelligent_exp e1) (pe_intelligent_exp e2))]))
+    [(Let x e1 e2) (Let x (pe_intelligent_exp e1) (pe_intelligent_exp e2))]
+    [(If e1 e2 e3) (If (pe_intelligent_exp e1) (pe_intelligent_exp e2) (pe_intelligent_exp e3))]))
 
-(define (pe_Lvar p)
+(define (pe_Lif p)
   (match p
     [(Program '() e) (Program '() (pe_intelligent_exp (pe_exp e)))]))
 
@@ -403,7 +406,7 @@
 (define compiler-passes
   `(  ("shrink", shrink, interp-Lif, type-check-Lif)
       ("uniquify" ,uniquify ,interp-Lif ,type-check-Lif)
-      ; ("patial evaluator Lvar" ,pe_Lvar ,interp-Lvar)
+      ("patial evaluator Lvar" ,pe_Lif ,interp-Lif ,type-check-Lif)
      ;; Uncomment the following passes as you finish them.
     ;  ("remove complex opera*" ,remove-complex-opera* ,interp-Lvar)
     ;  ("explicate control" ,explicate-control ,interp-Cvar)
