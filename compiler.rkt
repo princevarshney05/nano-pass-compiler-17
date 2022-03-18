0a65c3fb052b138fa6edfe48c49acb59852e2cfe
vjspranav <pranavasri@live.in>
2022-03-18 05:46:01 +0530

    P22: Extend remove-complex-opera* for Bool and If

--- compiler.rkt
+++ compiler.rkt
@@ -59,9 +59,15 @@
   (match e
     [(Int n) (values (Int n) '())]
     [(Var x) (values (Var x) '())]
+    [(Bool t) (values (Bool t) '())]
     [(Let key val body)
      (define-values (body-atom body-env) (rco-atom body))
      (values body-atom (cons (list key (rco-exp val)) body-env))]
+    ; It is particularly important to not replace its condition with a temporary variable because 
+    ; that would interfere with the generation of high-quality output in the upcoming explicate_control pass
+    [(If e1 e2 e3)  
+     (define key (gensym))
+     (values (Var key) `((,key ,(rco-exp e))))]
     [(Prim op es)
      (define-values (new-exp new-env) (for/lists (l1 l2) ([e es]) (rco-atom e)))
      (define key (gensym))
@@ -79,7 +85,10 @@
   (match e
     [(Int n) (Int n)]
     [(Var x) (Var x)]
+    [(Bool t) (Bool t)]
     [(Let key val body) (Let key (rco-exp val) (rco-exp body))]
+    ; Should I add rco-exp for e1 too?
+    [(If e1 e2 e3) (If e1 (rco-exp e2) (rco-exp e3))]
     [(Prim op es)
      (define-values (new-exp combined-list-env) (for/lists (l1 l2) ([e es]) (rco-atom e)))
      (normalise-env-exp (append* combined-list-env) (Prim op new-exp))]))
