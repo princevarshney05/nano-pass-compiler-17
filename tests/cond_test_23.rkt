10e698abfb2c51742c440b52c8cf371d38e4bf6f
vjspranav <pranavasri@live.in>
2022-04-01 12:28:12 +0530

    Add all test cases for explicate control for Lif

---
+++ tests/cond_test_23.rkt
@@ -0,0 +1,12 @@
+; Ex 23 - 3
+(if 
+    (if 
+        (if 
+            (if #f #t (not (< 2 3)))
+            (> (read) 2)
+            (eq? 1 1))
+        (let ([y (read)]) (eq? y 2))
+        (not (< 3 2)))
+    1
+    2
+    )
