763811426dd00b29d2e00fbb1d4a58f1c8de1cb6
vjspranav <pranavasri@live.in>
2022-04-15 13:27:56 +0530

    Add a few test cases for while

--- run-tests.rkt
+++ run-tests.rkt
@@ -25,11 +25,11 @@
           (string=? r (car (string-split p "_"))))
         all-tests)))
 
-;(interp-tests "var" #f compiler-passes interp-Lvec "var_test" (tests-for "var"))
-;(interp-tests "cond" type-check-Lvec compiler-passes interp-Lvec "cond_test" (tests-for "cond"))
-;(interp-tests "my" type-check-Lvec compiler-passes interp-Lvec "my_test" (tests-for "my"))
-;(interp-tests "while" type-check-Lvec compiler-passes interp-Lvec "while_test" (tests-for "while"))
-;(interp-tests "vectors" type-check-Lvec compiler-passes interp-Lvec "vectors_test" (tests-for "vectors"))
+; (interp-tests "var" #f compiler-passes interp-Lvec "var_test" (tests-for "var"))
+; (interp-tests "cond" type-check-Lvec compiler-passes interp-Lvec "cond_test" (tests-for "cond"))
+; (interp-tests "my" type-check-Lvec compiler-passes interp-Lvec "my_test" (tests-for "my"))
+(interp-tests "while" type-check-Lvec compiler-passes interp-Lvec "while_test" (tests-for "while"))
+(interp-tests "vectors" type-check-Lvec compiler-passes interp-Lvec "vectors_test" (tests-for "vectors"))
 
 ; Single Interp test
 (interp-tests "single" type-check-Lvec compiler-passes interp-Lvec "single_test" (tests-for "single"))
--- tests/while_test_1.rkt
+++ tests/while_test_1.rkt
@@ -1,2 +1,5 @@
 (let ([x 2])
-   (+ x (begin (set! x 40) x))) 
+  (+ x
+     (begin
+       (set! x 40)
+       x)))
